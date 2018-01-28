b6789d76246bff6e653373df3afba59b628d1773
Lami Kabir <asaadullamikabir@gmail.com>
2018-01-27 23:25:38 -0500

    forgot to add the two files I modified which is basic-input.js and hello-world.js

--- src/examples/basic-input.js
+++ src/examples/basic-input.js
@@ -3,20 +3,41 @@ class BasicInput extends React.Component {
   constructor(props) {
     super(props);
     this.handleChange = this.handleChange.bind(this);
+    this.insertList = this.insertList.bind(this);
+   // this.theList = this.theList.bind(this);
+    //maybe you dont need the comas
     this.state = {
-      userInput: ''
+      userInput: '',
+      list: []
     };
   }
   handleChange(e) {
     this.setState({userInput: e.target.value});
   }
+  insertList(){
+      this.state.list.push(<li>{this.state.userInput}</li>);
+
+   // this.state.list.push(this.state.userInput);
+     //console.log(this.state.list);
+    this.setState({list: this.state.list});
+  }
+ /* theList(){
+     let updatedList = this.state.list.map((key) =>
+    <li>{value}</li>
+    );
+    
+    return updatedList;
+  }*/
   render() {
     return (
       <div>
           <p>
             Please enter some input
           </p>
-          <input onChange={this.handleChange}/>
+          <input onChange={this.handleChange}/><button onClick={this.insertList} type="submit">Add</button>
+          <ul>
+           {this.state.list}
+          </ul>
           <p>
             You entered: {this.state.userInput}
           </p>
@@ -34,3 +55,8 @@ const renderBasicInput = () => {
     document.getElementById('root')
   );
 };
+
+
+/*{for(i=0; i < this.state.list; i++;){
+  
+              }}*/
